55b1e2e5d3205789825b1911942ccb79f029c09f
nanana4 <e245724@ie.u-ryukyu.ac.jp>
2024-12-18 13:00:39 +0900

    step3

--- week9_report5/Report5/src/Step1.java
+++ week9_report5/Report5/src/Step1.java
@@ -1,14 +1,7 @@
 public class Step1 {
     public static void main(String[] args) {
-    String str = null;
-    
-        try {
-            System.out.println(str.length());
-        }
-
-        catch (NullPointerException e) {
-            System.out.println("NullPointerExceptionエラー");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
     }
 }
